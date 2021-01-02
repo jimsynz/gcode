a7d970443b141ed47e774c46174603185e654fec
James Harton <james@harton.nz>
2021-01-02 18:24:55 +1300

    chore: Create empty mix project.

---
+++ lib/gcode.ex
@@ -0,0 +1,18 @@
+defmodule Gcode do
+  @moduledoc """
+  Documentation for `Gcode`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gcode.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,47 @@
+defmodule Gcode.MixProject do
+  use Mix.Project
+
+  @version "0.1.0"
+  @description """
+  A G-code parser and generator.
+  """
+
+  def project do
+    [
+      app: :gcode,
+      version: @version,
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      package: package(),
+      description: @description,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  def package do
+    [
+      maintainers: ["James Harton <james@harton.nz>"],
+      licenses: ["Hippocratic"],
+      links: %{
+        "Source" => "https://gitlab.com/jimsy/gcode"
+      }
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, ">= 0.0.0", only: ~w[dev test]a, runtime: false},
+      {:earmark, ">= 0.0.0", only: ~w[dev test]a, runtime: false},
+      {:credo, "~> 1.1", only: ~w[dev test]a, runtime: false},
+      {:git_ops, "~> 2.3", only: ~w[dev test]a, runtime: false}
+    ]
+  end
+end
