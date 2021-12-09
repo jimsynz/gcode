9f511e3a0a12c1b92a0495379b222093ba670eb2
James Harton <james@harton.nz>
2021-12-09 17:29:44 +1300

    chore(deps): update dependency git_ops to ~> 2.4

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Gcode.MixProject do
       {:ex_doc, ">= 0.26.0", only: ~w[dev test]a, runtime: false},
       {:earmark, ">= 1.4.19", only: ~w[dev test]a, runtime: false},
       {:credo, "~> 1.6", only: ~w[dev test]a, runtime: false},
-      {:git_ops, "~> 2.3", only: ~w[dev test]a, runtime: false},
+      {:git_ops, "~> 2.4", only: ~w[dev test]a, runtime: false},
       {:nimble_parsec, "~> 1.1"},
       {:parallel_stream, "~> 1.0"}
     ]
