a6da6c0a4f77c02167d6d61ffc6e9476af622455
James Harton <james@harton.nz>
2022-02-21 11:38:13 +1300

    chore(deps): update dependency ex_doc to >= 0.28.1

--- mix.exs
+++ mix.exs
@@ -41,8 +41,8 @@ defmodule Gcode.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, ">= 0.26.0", only: ~w[dev test]a, runtime: false},
-      {:earmark, ">= 1.4.20", only: ~w[dev test]a, runtime: false},
+      {:ex_doc, "~> 0.28", only: ~w[dev test]a, runtime: false},
+      {:earmark, "~> 1.4", only: ~w[dev test]a, runtime: false},
       {:credo, "~> 1.6", only: ~w[dev test]a, runtime: false},
       {:git_ops, "~> 2.4", only: ~w[dev test]a, runtime: false},
       {:nimble_parsec, "~> 1.2"},
