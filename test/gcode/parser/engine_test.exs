93c316567893071adb758d1511d8a8e4605a4997
James Harton <james@harton.nz>
2023-07-09 14:04:30 +1200

    chore: run formatter.

--- test/gcode/parser/engine_test.exs
+++ test/gcode/parser/engine_test.exs
@@ -5,36 +5,39 @@ defmodule Gcode.Parser.EngineTest do
   import FixtureHelper
   @moduledoc false
 
-  it_parses_into("%", tape: '')
-  it_parses_into("% hello", tape: 'hello')
-  it_parses_into("()", comment: '')
-  it_parses_into("(hello)", comment: 'hello')
-  it_parses_into("( hello )", comment: 'hello')
-  it_parses_into("; hello", comment: 'hello')
-  it_parses_into("G0", block: [word: [command: 'G', address: [integer: '0']]])
-  it_parses_into("G 0", block: [word: [command: 'G', address: [integer: '0']]])
-  it_parses_into("G54.1", block: [word: [command: 'G', address: [float: '54.1']]])
-  it_parses_into("G 54.1", block: [word: [command: 'G', address: [float: '54.1']]])
-  it_parses_into("G-1", block: [word: [command: 'G', address: [{:-, [integer: '1']}]]])
-  it_parses_into("G+1", block: [word: [command: 'G', address: [{:+, [integer: '1']}]]])
-  it_parses_into("G!1", block: [word: [command: 'G', address: [{:!, [integer: '1']}]]])
+  it_parses_into("%", tape: ~c"")
+  it_parses_into("% hello", tape: ~c"hello")
+  it_parses_into("()", comment: ~c"")
+  it_parses_into("(hello)", comment: ~c"hello")
+  it_parses_into("( hello )", comment: ~c"hello")
+  it_parses_into("; hello", comment: ~c"hello")
+  it_parses_into("G0", block: [word: [command: ~c"G", address: [integer: ~c"0"]]])
+  it_parses_into("G 0", block: [word: [command: ~c"G", address: [integer: ~c"0"]]])
+  it_parses_into("G54.1", block: [word: [command: ~c"G", address: [float: ~c"54.1"]]])
+  it_parses_into("G 54.1", block: [word: [command: ~c"G", address: [float: ~c"54.1"]]])
+  it_parses_into("G-1", block: [word: [command: ~c"G", address: [{:-, [integer: ~c"1"]}]]])
+  it_parses_into("G+1", block: [word: [command: ~c"G", address: [{:+, [integer: ~c"1"]}]]])
+  it_parses_into("G!1", block: [word: [command: ~c"G", address: [{:!, [integer: ~c"1"]}]]])
 
   it_parses_into("G1 X112.518 Y131.525 E59.51636 (hello)",
     block: [
-      word: [command: 'G', address: [integer: '1']],
-      word: [command: 'X', address: [float: '112.518']],
-      word: [command: 'Y', address: [float: '131.525']],
-      word: [command: 'E', address: [float: '59.51636']],
-      comment: 'hello'
+      word: [command: ~c"G", address: [integer: ~c"1"]],
+      word: [command: ~c"X", address: [float: ~c"112.518"]],
+      word: [command: ~c"Y", address: [float: ~c"131.525"]],
+      word: [command: ~c"E", address: [float: ~c"59.51636"]],
+      comment: ~c"hello"
     ]
   )
 
   it_parses_into("M82 ;absolute extrusion mode",
-    block: [word: [command: 'M', address: [integer: '82']], comment: 'absolute extrusion mode']
+    block: [
+      word: [command: ~c"M", address: [integer: ~c"82"]],
+      comment: ~c"absolute extrusion mode"
+    ]
   )
 
   it_parses_into("M117 Hello world",
-    block: [word: [command: 'M', address: [integer: '117']], string: 'Hello world']
+    block: [word: [command: ~c"M", address: [integer: ~c"117"]], string: ~c"Hello world"]
   )
 
   it_parses_into(read_fixture("fusion_360_milling_grbl.nc"), fn tokens ->
